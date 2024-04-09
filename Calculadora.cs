f1ecd664d865f8ae5f9188c481dc383c9df2854e
Alex Paiano <alexpaiano14@outlook.com>
2024-04-08 22:26:45 -0300

    Practica TDD

---
+++ Calculadora.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraString
+{
+    public class Calculadora
+    {
+        public int Sumar(string numeros) 
+        {
+            if (string.IsNullOrEmpty(numeros)) return 0;
+
+            string[] numerosSeparados = numeros.Split(',');
+            int suma = 0;
+            foreach (string numero in numerosSeparados)
+            {
+                suma += int.Parse(numero);
+            }
+            return suma;
+        } 
+    }
+}
